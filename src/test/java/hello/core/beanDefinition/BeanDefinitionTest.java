79ce4e2bc0ade18dbd68bbba4ba8adf0d44019c7
nova1113 <nova9353@gmail.com>
2022-01-10 22:37:22 +0900

    Test BeanDefinitionTest

---
+++ src/test/java/hello/core/beanDefinition/BeanDefinitionTest.java
@@ -0,0 +1,31 @@
+package hello.core.beanDefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+//    자바 Config파일로 Bean을 등록하는 것은 -> factoryMethod를 사용해서 Bean을 생섵ㅇ한다.
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//    ApplicationContext.getBeanDefinition은 없음
+//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for(String beanDefinitionName : beanDefinitionNames){
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
