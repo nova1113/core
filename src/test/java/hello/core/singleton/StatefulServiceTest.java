9bca2290827506d199f2217bdc21948fae734fec
nova1113 <nova9353@gmail.com>
2022-01-11 23:28:07 +0900

    Test StatefulService

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,39 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //웹에서 요청이 오면 각각 쓰레드에 관리가 된다.
+        //ThreadA : A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB : B사용자 20000원 주문
+        statefulService2.order("userA", 20000);
+
+        //ThreadA : 사용자A 주문 금액을 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+}
