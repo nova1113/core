85678b3e9c7a310b5bbde77dcd01575c2c1ad0e8
nova1113 <nova9353@gmail.com>
2022-01-11 23:36:27 +0900

    Refactor StatefulService  to Stateless

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -18,15 +18,16 @@ class StatefulServiceTest {
 
         //웹에서 요청이 오면 각각 쓰레드에 관리가 된다.
         //ThreadA : A사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
+
         //ThreadB : B사용자 20000원 주문
-        statefulService2.order("userA", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA : 사용자A 주문 금액을 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        assertThat(userAPrice).isEqualTo(10000);
     }
 
     static class TestConfig {
