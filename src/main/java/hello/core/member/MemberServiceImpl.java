08d14eabb97718029c4aa1dca6d11e1e6a119bb9
nova1113 <nova9353@gmail.com>
2022-01-24 12:40:33 +0900

    Change Annotation @ComponentScan And @Autowired

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements  MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired // ac.getBean(MemberRepository.class)코드가 들어가있다고 생각하면 됨
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
