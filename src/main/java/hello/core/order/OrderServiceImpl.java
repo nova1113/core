6a691291160c43f42665d33d7790932593132ed5
nova1113 <nova9353@gmail.com>
2022-04-11 20:25:49 +0900

    Learn Qualifier And Primary Annotation

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,10 +8,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor        //final이 붙은 필드를 파라미터로 받는 생성자를 만들어 줌
+//@RequiredArgsConstructor        //final이 붙은 필드를 파라미터로 받는 생성자를 만들어 줌
 public class OrderServiceImpl implements OrderService{
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
@@ -20,12 +21,16 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
 
+//    @Autowired
+//    private DiscountPolicy rateDiscountPolicy;
+
 //      생성자가 단 하나만 있을 경우 Autowired생략해도 생성자로 주입
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        System.out.println("Constructor");
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("Constructor");
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
     @Override
